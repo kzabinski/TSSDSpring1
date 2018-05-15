6b52a933b1f5c645d96de67ec7e878a368e8f270
krzysztofzabinski <krzysztof.kamil.zabinski@gmail.com>
2018-05-15 22:00:27 +0200

    First commit

---
+++ MVCwebpage/src/main/java/com/kzabinski/hello/MvCwebpageApplication.java
@@ -0,0 +1,12 @@
+package com.kzabinski.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvCwebpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvCwebpageApplication.class, args);
+	}
+}
